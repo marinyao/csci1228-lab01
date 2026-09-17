a32aa565ee7c7d79f59a58eca523742b98636030
s1824872 <s1824872@10.206.141.38>
2026-09-17 12:19:26 -0300

    Add braces so else matches intended if

--- src/Trace3.java
+++ src/Trace3.java
@@ -12,13 +12,17 @@ public class Trace3 {
     public static void main(String[] args) {
         int score = 45;
 
-        if (score > 90)
-            if (score > 95)
+        if (score > 90) {
+            if (score > 95) {
                 System.out.println("A+");
-        else
-            System.out.println("Not an A");
+            }
+        } else {
+                System.out.println("Not an A");
 
-        System.out.println("done");
+                System.out.println("done");
+            }
+
+        }
+
     }
-}
 
